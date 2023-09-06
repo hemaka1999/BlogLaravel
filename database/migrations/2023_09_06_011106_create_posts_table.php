f38bb95319a1817035594787102e1ef153904222
hemaka1999 <hemaka1999@gmail.com>
2023-09-06 07:22:26 +0530

    Admin Post Category

---
+++ database/migrations/2023_09_06_011106_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 2048);
+            $table->string('slug', 2048);
+            $table->string('thumbnail', 2048)->nullable();
+            $table->string('body');
+            $table->boolean('active');
+            $table->datetime('published_at');
+            $table->foreignIdFor(\App\Models\User::class, 'user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
