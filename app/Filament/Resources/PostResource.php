87ba789614bc2f0d56edd0c3ddd352ff3cf4bcc2
hemaka1999 <hemaka1999@gmail.com>
2023-09-06 18:04:36 +0530

    Dashboard update1

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -52,8 +52,7 @@ class PostResource extends Resource
                     ->maxLength(255),
                 Forms\Components\Toggle::make('active')
                     ->required(),
-                Forms\Components\DateTimePicker::make('published_at')
-                    ->required(),
+                Forms\Components\DateTimePicker::make('published_at'),
 
                 ])->columnspan(8),
 
@@ -76,16 +75,16 @@ class PostResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('title'),
-                Tables\Columns\TextColumn::make('slug'),
-                Tables\Columns\TextColumn::make('thumbnail'),
-                Tables\Columns\TextColumn::make('body'),
+                // Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\ImageColumn::make('thumbnail'),
+                // Tables\Columns\TextColumn::make('body'),
                 Tables\Columns\IconColumn::make('active')
                     ->boolean(),
                 Tables\Columns\TextColumn::make('published_at')
                     ->dateTime(),
-                Tables\Columns\TextColumn::make('user.name'),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime(),
+                // Tables\Columns\TextColumn::make('user.name'),
+                // Tables\Columns\TextColumn::make('created_at')
+                //     ->dateTime(),
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime(),
             ])
